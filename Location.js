8635aad557c2be6d9f8d410e0f0ff7fe48922a4c
softrain <yurimplus@naver.com>
2021-06-30 16:47:53 +0900

    Add : 지역별

---
+++ Location.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import "react-native-gesture-handler";
+
+const Location = ({ navigation }) => {
+  return (
+    <View style={styles.body}>
+      <View style={styles.container1}>
+        <Text style={styles.header}>💉똑똑 선별진료소💉</Text>
+      </View>
+      <View style={styles.container2}>
+        <Text>주소 검색 창</Text>
+        <TouchableOpacity
+          style={styles.menu}
+          onPress={() => navigation.navigate("Clinic")}
+        >
+          <Text style={styles.text2}>다음 화면으로 넘어감</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footer}>Enonymous</Text>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  container1: {
+    flex: 1,
+    backgroundColor: "#00462a",
+    alignItems: "center",
+    marginTop: 0,
+    height: 5,
+  },
+  container2: {
+    flex: 6,
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  header: {
+    color: "white",
+    fontSize: 30,
+    letterSpacing: 3,
+    fontWeight: "bold",
+    height: 50,
+    margin: 55,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  menu: {
+    marginTop: 50,
+    fontSize: 30,
+    borderColor: "#00462a",
+    backgroundColor: "#00462a",
+    margin: 10,
+    borderWidth: 2,
+    borderRadius: 15,
+    width: "70%",
+    height: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text2: {
+    color: "white",
+    fontSize: 30,
+  },
+  footer: {
+    flex: 0.8,
+    color: "white",
+    fontSize: 30,
+    letterSpacing: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00462a",
+  },
+});
+export default Location;
