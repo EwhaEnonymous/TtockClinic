ec80fc346bb013f4813319f49eb89368e3e22b5e
softrain <yurimplus@naver.com>
2021-07-22 04:34:38 +0900

    구글맵 구현 중

--- currentLocation.js
+++ currentLocation.js
@@ -5,12 +5,9 @@ import "react-native-gesture-handler";
 import { PROVIDER_GOOGLE } from "react-native-maps";
 import MapView from "react-native-maps";
 import * as Location from "expo-location";
-<<<<<<< HEAD
 //search
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-=======
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import constants from "./constants";
->>>>>>> 53c29e9638919ce48c5e65513389b06b78d48979
 
 function currentLocation({ navigation }) {
   const [location, setLocation] = useState(null);
@@ -73,15 +70,19 @@ function currentLocation({ navigation }) {
               }}
             />
             <GooglePlacesAutocomplete
-              placeholder='Search'
+              placeholder="Search"
               onPress={(data, details = null) => {
                 // 'details' is provided when fetchDetails = true
                 console.log(data, details);
               }}
+              listViewDisplayed={"auto"}
+              fetchDetails={true}
+              getDefaultValue="선별진료소"
+              renderDescription={(row) => row.description}
               query={{
                 key: "AIzaSyBbTEHujkplPpDLpPwyhAGC-9yywmDuaCo",
-                language: 'ko',
-                components: 'country:ko',
+                language: "ko",
+                components: "country:ko",
               }}
             />
           </MapView>
